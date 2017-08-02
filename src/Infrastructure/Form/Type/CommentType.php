5f847ee2576062d7a870dc7a31b92cdbb5d9aa71
Łukasz Chruściel <lchrusciel@gmail.com>
2017-08-02 09:50:01 +0200

    [UI] [Administrator] Return types in form

--- src/Infrastructure/Form/Type/CommentType.php
+++ src/Infrastructure/Form/Type/CommentType.php
@@ -12,12 +12,12 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 final class CommentType extends AbstractType
 {
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('message', TextareaType::class);
     }
 
-    public function configureOptions(OptionsResolver $resolver)
+    public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults(['data_class' => Comment::class]);
     }
