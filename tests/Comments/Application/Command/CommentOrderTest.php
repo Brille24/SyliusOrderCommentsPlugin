5487752fb2779b18dbae0e99479d341c421ac570
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2019-01-23 11:30:26 +0100

    fixed_phpunit again

--- tests/Comments/Application/Command/CommentOrderTest.php
+++ tests/Comments/Application/Command/CommentOrderTest.php
@@ -28,10 +28,10 @@ final class CommentOrderTest extends TestCase
      */
     public function it_has_option_file_path_and_file_type(): void
     {
-        $fileName = 'text.txt';
+        $filePath = __DIR__.'/../../Infrastructure/Form/Type/text.txt';
 
         // Symfony 3.4 style
-        $file = new UploadedFile($fileName, $fileName, null, null, null, true);
+        $file = new UploadedFile($filePath, $filePath, null, null, null, true);
         $command = CommentOrder::create('#00002', 'test@test.com', 'Hello', true, $file);
 
         $this->assertEquals($file, $command->file());
