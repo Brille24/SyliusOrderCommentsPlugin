292107efd44b0f957d5b12a97e2baa67c3c117cd
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2023-03-16 15:17:39 +0100

    Require Sylius ^1.11

--- src/Infrastructure/Brille24SyliusOrderCommentsPlugin.php
+++ src/Infrastructure/Brille24SyliusOrderCommentsPlugin.php
@@ -8,4 +8,9 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 final class Brille24SyliusOrderCommentsPlugin extends Bundle
 {
     use SyliusPluginTrait;
+
+    public function getPath(): string
+    {
+        return \dirname(__DIR__);
+    }
 }
