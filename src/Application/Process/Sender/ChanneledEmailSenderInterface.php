b039027ad68d05f1eb6d781fd856c6cf063137d9
Jakob Tolkemit <jakob.tolkemit@brille24.de>
2023-03-27 12:07:19 +0200

    Allow DataPart attachments

--- src/Application/Process/Sender/ChanneledEmailSenderInterface.php
+++ src/Application/Process/Sender/ChanneledEmailSenderInterface.php
@@ -5,12 +5,13 @@ declare(strict_types=1);
 namespace Brille24\OrderCommentsPlugin\Application\Process\Sender;
 
 use Sylius\Component\Mailer\Sender\SenderInterface;
+use Symfony\Component\Mime\Part\DataPart;
 
 interface ChanneledEmailSenderInterface extends SenderInterface
 {
     /**
      * @param string[]|null[] $recipients A list of email addresses to receive the message. Providing null or empty string in the list of recipients is deprecated and will be removed in SyliusMailerBundle 2.0
-     * @param string[] $attachments A list of file paths to attach to the message.
+     * @param string[]|DataPart[] $attachments A list of file paths to attach to the message.
      * @param string[] $replyTo A list of email addresses to set as the Reply-To address for the message.
      * @param string[] $ccRecipients A list of email addresses set as carbon copy
      * @param string[] $bccRecipients A list of email addresses set as blind carbon copy
